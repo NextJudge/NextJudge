022ecb03b97adf0e1d8ea8d172d832d58ddb639d
Thomas <nyumat18@gmail.com>
2024-02-02 20:09:29 -0800

    refactor: API + UserService classes

---
+++ src/bridge/app/src/ApiService.ts
@@ -0,0 +1,22 @@
+class ApiService {
+  static async get(url: string): Promise<Response> {
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  static async post(url: string, body: object): Promise<Response> {
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+  }
+}
+
+export default ApiService;
