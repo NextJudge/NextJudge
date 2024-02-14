357daafe2d4a77d8cf4e4e8c61a79aa3deb99be1
OB <obarroncs@gmail.com>
2024-02-13 18:16:02 -0800

    Remove old stuff

--- src/engine/languages/Cplusplus/init.sh
+++
@@ -1,5 +0,0 @@
-i=$1
-date +%s%N > /app/time$i.txt
-timeout $2 bash -c "( cat /app/in$i.txt | ( g++ -o /app/Main /app/Main.cpp -pthread -lm && /app/Main ) ) 2> /app/error$i.txt 1> /app/output$i.txt"
-echo $? > /app/exitCode$i.txt
-date +%s%N >> /app/time$i.txt
