49efc32f804fa662caa96b30b31ef6ceb050f29c
Thomas <nyumat18@gmail.com>
2024-03-18 01:35:51 -0700

    OpenGraph Image

--- src/web/src/app/layout.tsx
+++ src/web/src/app/layout.tsx
@@ -8,7 +8,25 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "NextJudge",
-  description: "A Next Generation Platform for Competitive Programming.",
+  description:
+    "An all-new, competitive programming platform built for organizers, developers, and participants.",
+  openGraph: {
+    type: "website",
+    url: "https://nextjudge.vercel.app/",
+    title: "NextJudge - Elevate your coding skills to the next level.",
+    description:
+      "An all-new, competitive programming platform built for organizers, developers, and participants.",
+    siteName: "NextJudge",
+    images: [
+      {
+        url: "https://nextjudge.vercel.app/og.png",
+        secureUrl: "https://nextjudge.vercel.app/og.png",
+        width: 2880,
+        height: 1612,
+        alt: "NextJudge - Elevate your coding skills to the next level.",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
