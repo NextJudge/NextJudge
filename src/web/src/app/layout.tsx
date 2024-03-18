712c66281eabbb1c0ff9d51c9d7c17d2008904ce
Thomas <nyumat18@gmail.com>
2024-03-18 01:45:54 -0700

    Improved SEO w/ og and Added Favicon

--- src/web/src/app/layout.tsx
+++ src/web/src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NextJudge",
+  title: "NextJudge - Elevate your coding skills to the next level.",
   description:
     "An all-new, competitive programming platform built for organizers, developers, and participants.",
   openGraph: {
