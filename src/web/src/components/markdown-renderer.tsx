c1764653518e7132560dfc3eba8032fa5f290afc
Thomas <nyumat18@gmail.com>
2024-05-18 14:41:11 -0700

    example env file

--- src/web/src/components/markdown-renderer.tsx
+++ src/web/src/components/markdown-renderer.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-// import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
+import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
 import Markdown from "react-markdown";
-// import rehypeKatex from "rehype-katex";
-// import rehypeStringify from "rehype-stringify";
-// import remarkMath from "remark-math";
-// import remarkParse from "remark-parse";
-// import remarkRehype from "remark-rehype";
-// import rehypeRaw from "rehype-raw";
+import rehypeKatex from "rehype-katex";
+import rehypeStringify from "rehype-stringify";
+import remarkMath from "remark-math";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeRaw from "rehype-raw";
 
 export default function MarkdownRenderer({ markdown }: { markdown: string }) {
   return (
     <>
       <Markdown
-
-      // remarkPlugins={[remarkMath, remarkParse, remarkRehype]}
-      // rehypePlugins={[rehypeKatex, rehypeStringify, rehypeRaw]}
+        remarkPlugins={[remarkMath, remarkParse, remarkRehype]}
+        rehypePlugins={[rehypeKatex, rehypeStringify, rehypeRaw]}
       >
         {markdown}
       </Markdown>
