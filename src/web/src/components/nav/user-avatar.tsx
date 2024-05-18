27190a8918cf23370f744ae18fd59dfe6112d3e0
Thomas <nyumat18@gmail.com>
2024-05-18 04:22:30 -0700

    nav: add navigation components

---
+++ src/web/src/components/nav/user-avatar.tsx
@@ -0,0 +1,52 @@
+import { auth } from "@/app/auth";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function UserAvatar() {
+  const session = await auth();
+  // TODO: Remove this console.log
+  console.log(session);
+  if (!session?.user) return null;
+  if (!session.user.image) return null;
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="overflow-hidden rounded-full"
+          >
+            <Image
+              src={session.user.image}
+              width={36}
+              height={36}
+              alt="Avatar"
+              className="overflow-hidden rounded-full"
+            />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>{session.user.name}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <Link href="/auth/logout">
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </Link>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
