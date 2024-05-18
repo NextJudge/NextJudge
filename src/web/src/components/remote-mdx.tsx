c8de772cc0d8008d1e9d98d5ea262ef75dc8218f
Thomas <nyumat18@gmail.com>
2024-05-18 04:16:24 -0700

    chore: organize files

--- src/web/src/components/remote-mdx.tsx
+++
@@ -1,7 +0,0 @@
-import { MDXRemote } from "next-mdx-remote/rsc";
-
-export default async function RemoteMdxPage() {
-  const res = await fetch("../md/twosum.md");
-  const markdown = await res.text();
-  return <MDXRemote source={markdown} />;
-}
