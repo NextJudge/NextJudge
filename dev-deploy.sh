8fcbc1e93196a316325aee2248372c4061c99ecb
OB <obarroncs@gmail.com>
2024-05-25 14:10:27 -0700

    new deploy script taking into account elastic

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Build the basejudge
-cd $SCRIPT_DIR/src/judge
-docker build -f Dockerfile.newbase --target dev -t basejudge:dev .
 
-cd $SCRIPT_DIR/src/web
-if [[ "$*" == *"webprod"* ]]
+if [[ "$*" == *"nojudge"* ]]
 then
-    npm start &
+    echo "Not starting judge"
 else
-    npm run dev &
+    # Build the basejudge
+    cd $SCRIPT_DIR/src/judge
+    docker build -f Dockerfile.newbase --target dev -t basejudge:dev .
+fi
+
+if [[ "$*" == *"web"* ]]
+then
+    cd $SCRIPT_DIR/src/web
+    if [[ "$*" == *"webprod"* ]]
+    then
+        npm start &
+    else
+        npm run dev &
+    fi
 fi
 
 
+PROFILE_STRING=""
+
+if [[ "$*" == *"noelastic"* ]]
+then
+    echo "Not starting elastic"
+else
+    PROFILE_STRING="--profile elastic"
+fi
+
+if ! [[ "$*" == *"nojudge"* ]]
+then
+    PROFILE_STRING="$PROFILE_STRING --profile judge"
+fi
 
 cd $SCRIPT_DIR
 # Start all services
-docker compose -f docker-compose.dev.yml up --build
+if [[ "$*" == *"noelastic"* ]]
+then
+    ELASTIC_ENABLED="false" docker compose $PROFILE_STRING -f docker-compose.dev.yml up --build
+else
+    ELASTIC_ENABLED="true" docker compose $PROFILE_STRING -f docker-compose.dev.yml up --build
+fi
