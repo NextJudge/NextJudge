b74894a1aca0d2f242bb3386c438dc28db7257fe
OB <obarroncs@gmail.com>
2024-05-25 14:16:02 -0700

    windows

--- win-dev-deploy.ps1
+++ win-dev-deploy.ps1
@@ -3,14 +3,14 @@ param (
     [string]$nojudge = "Whether or not to spin up the judge"
 )
 
-if ($Args.Count -gt 0) {
+if ($nojudge) {
+    docker-compose -f docker-compose.dev.yml up --build nextjudge-bridge rabbitmq nextjudge-data-layer db
+} else {
     cd $PSScriptRoot/src/judge
     docker build -f Dockerfile.newbase --target dev -t basejudge:dev .
 
     cd $PSScriptRoot
-    docker-compose -f docker-compose.dev.yml up --build
-} else {
-    docker-compose -f docker-compose.dev.yml up --build nextjudge-bridge rabbitmq nextjudge-data-layer db elasticsearch
+    docker-compose -f docker-compose.dev.yml up --build nextjudge-bridge rabbitmq nextjudge-data-layer db nextjudge-judge
 }
 
 
