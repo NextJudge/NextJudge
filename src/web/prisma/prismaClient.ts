998b55373923b7007e5db3092cfff6277d3c9ac7
Thomas <nyumat18@gmail.com>
2024-05-26 20:32:22 -0700

    fix prisma too many connections bug

--- src/web/prisma/prismaClient.ts
+++ src/web/prisma/prismaClient.ts
@@ -1,5 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+// @ts-nocheck
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+
+  prisma = global.prisma;
+}
 
 export default prisma;
