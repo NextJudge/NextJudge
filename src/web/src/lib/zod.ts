b8bf2e9710b208b59881d09a0442aacc4fa304b5
dotcomnerd <Tomxxxfresh@gmail.com>
2025-08-26 18:57:07 -0700

    chore(auth): require name during sign-up

--- src/web/src/lib/zod.ts
+++ src/web/src/lib/zod.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const AuthorizeSchema = z
   .object({
+    name: z.string().min(2, "Name must be at least 2 characters"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z
