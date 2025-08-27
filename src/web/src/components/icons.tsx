1d22eda74d0a81c68e920122b0a257bbb9764d8d
dotcomnerd <Tomxxxfresh@gmail.com>
2025-08-27 01:11:30 -0700

    style(icons): add more lucide icons for common use-case

--- src/web/src/components/icons.tsx
+++ src/web/src/components/icons.tsx
@@ -1,6 +1,18 @@
 import { EnterIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 import { IconBrandGithub, IconBrandGoogle } from "@tabler/icons-react";
-import { EyeIcon, EyeOffIcon, Loader2Icon, Pyramid, Shell } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  CalendarIcon,
+  ClockIcon,
+  CopyIcon,
+  EyeIcon,
+  EyeOffIcon,
+  HelpCircleIcon,
+  Loader2Icon,
+  Pyramid,
+  Shell,
+  UsersIcon
+} from "lucide-react";
 
 export const Icons = {
   logo: Pyramid,
@@ -12,4 +24,10 @@ export const Icons = {
   enter: EnterIcon,
   altgithub: IconBrandGithub,
   google: IconBrandGoogle,
+  arrowLeft: ArrowLeftIcon,
+  calendar: CalendarIcon,
+  clock: ClockIcon,
+  users: UsersIcon,
+  copy: CopyIcon,
+  help: HelpCircleIcon,
 };
