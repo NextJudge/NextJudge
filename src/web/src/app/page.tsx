e7ccc5a730db668b552f7eb3382736af2d06aada
dotcomnerd <Tomxxxfresh@gmail.com>
2025-08-27 01:28:44 -0700

    style(landing): clean up background opacities and add session provider

--- src/web/src/app/page.tsx
+++ src/web/src/app/page.tsx
@@ -7,6 +7,7 @@ import { ScrollToTop } from "@/components/landing/scroll-up";
 import { Services } from "@/components/landing/services";
 import { Navbar } from "@/components/nav/navbar";
 import { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 const WhyNextJudge = dynamic(
@@ -23,43 +24,45 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <div className="fixed inset-0 pointer-events-none">
-        <Image
-          src="/blobs/blob1.svg"
-          alt="background"
-          priority={true}
-          width={100}
-          height={50}
-          className="dark:hidden absolute top-0 left-0 w-1/3 mx-auto opacity-70 h-auto bg-transparent blur-[200px] backdrop-filter"
-        />
-        <Image
-          src="/blobs/blob1.svg"
-          alt="background"
-          priority={true}
-          width={100}
-          height={50}
-          className="dark:hidden absolute top-0 right-0 w-3/6 opacity-50 h-auto bg-transparent blur-[200px] backdrop-filter"
-        />
-        <Image
-          src="/blobs/blob1.svg"
-          alt="background"
-          priority={true}
-          width={100}
-          height={50}
-          className="hidden dark:block dark:2xl:hidden absolute bottom-20 left-0 translate-y-2/4 w-full mx-auto opacity-70 h-full scale-100 bg-transparent blur-[250px] backdrop-filter"
-        />
-      </div>
-      <Navbar />
-      <main className="flex max-w-full gap-10 flex-col items-center justify-between overflow-x-hidden relative z-10">
-        <AltHero />
-        <WhyNextJudge />
-        <Services />
-        <LanguagesBanner />
-        <EarlyAccess />
-        <FAQ />
-        <Footer />
-        <ScrollToTop />
-      </main>
+      <SessionProvider>
+        <div className="fixed inset-0 pointer-events-none">
+          <Image
+            src="/blobs/blob1.svg"
+            alt="background"
+            priority={true}
+            width={100}
+            height={50}
+            className="absolute top-0 left-0 w-1/3 mx-auto opacity-70 dark:opacity-30 h-auto bg-transparent blur-[200px] backdrop-filter"
+          />
+          <Image
+            src="/blobs/blob1.svg"
+            alt="background"
+            priority={true}
+            width={100}
+            height={50}
+            className="absolute top-0 right-0 w-3/6 opacity-50 dark:opacity-20 h-auto bg-transparent blur-[200px] backdrop-filter"
+          />
+          <Image
+            src="/blobs/blob1.svg"
+            alt="background"
+            priority={true}
+            width={100}
+            height={50}
+            className="absolute bottom-20 left-0 translate-y-2/4 w-full mx-auto opacity-70 dark:opacity-30 h-full scale-100 bg-transparent blur-[250px] backdrop-filter"
+          />
+        </div>
+        <Navbar />
+        <main className="flex max-w-full gap-10 flex-col items-center justify-between overflow-x-hidden relative z-10">
+          <AltHero />
+          <WhyNextJudge />
+          <Services />
+          <LanguagesBanner />
+          <EarlyAccess />
+          <FAQ />
+          <Footer />
+          <ScrollToTop />
+        </main>
+      </SessionProvider>
     </>
   );
 }
