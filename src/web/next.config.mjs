690070abbff90c00f9109bb9456ddccdb8632bfc
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2025-12-12 23:19:14 -0800

    chore(web): add HTTPS configuration for html.tailus.io to next.js settings

--- src/web/next.config.mjs
+++ src/web/next.config.mjs
@@ -22,6 +22,12 @@ const nextConfig = {
         port: "",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "html.tailus.io",
+        port: "",
+        pathname: "**",
+      },
     ],
   },
 };
