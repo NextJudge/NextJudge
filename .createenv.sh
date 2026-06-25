5541bfc5c8bcd4d959317b4822489472b996ddc0
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2026-06-25 02:07:49 -0700

    chore(compose): split db and bridge secrets

--- .createenv.sh
+++ .createenv.sh
@@ -2,9 +2,9 @@
 
 cat <<EOF
 DB_PASSWORD=$(openssl rand -base64 64 | tr -d '\n/+=')
+WEB_BRIDGE_SECRET=$(openssl rand -base64 64 | tr -d '\n/+=')
 JUDGE_PASSWORD=$(openssl rand -base64 64 | tr -d '\n/+=')
 JWT_SIGNING_SECRET=$(openssl rand -base64 64 | tr -d '\n/+=')
-AUTH_PROVIDER_PASSWORD=$(openssl rand -base64 64 | tr -d '\n/+=')
 RABBITMQ_USER=$(openssl rand -base64 64 | tr -d '\n/+=')
 RABBITMQ_PASSWORD=$(openssl rand -base64 64 | tr -d '\n/+=')
 EOF
